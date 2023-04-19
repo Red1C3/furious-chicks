2323a6d081c5669e68b5c6efff340d7be81cc4ff
Mohammad Issawi <2000issawi@gmail.com>
2023-04-19 23:42:40 +0300

    Make BoxCullider only exist on surface voxels

--- Assets/Scripts/Voxels/VoxelGrid.cs
+++ Assets/Scripts/Voxels/VoxelGrid.cs
@@ -18,6 +18,7 @@ public class VoxelGrid : MonoBehaviour
         interiorVoxels = new List<Voxel>();
         origin = transform.position;
         buildGrid();
+        typeVoxels();
     }
 
     private void buildGrid()
@@ -41,7 +42,31 @@ public class VoxelGrid : MonoBehaviour
                     , Quaternion.identity, transform);
                     voxels[i][j][k].transform.localScale = new Vector3(voxelLen, voxelLen, voxelLen);
                     voxels[i][j][k].GetComponent<Voxel>().coords = new Vector3Int(i, j, k);
-                    voxels[i][j][k].GetComponent<BoxCullider>().updateBoundaries();
+                    voxels[i][j][k].GetComponent<Voxel>().grid=this;
+                }
+            }
+        }
+    }
+
+    private void typeVoxels()
+    {
+        for (int i = 0; i < voxels.Length; i++)
+        {
+            for (int j = 0; j < voxels[i].Length; j++)
+            {
+                for (int k = 0; k < voxels[i][j].Length; k++)
+                {
+                    Vector3 c = voxels[i][j][k].GetComponent<Voxel>().coords;
+                    if (c.x == 0 || c.x == voxels.Length - 1 || c.y == 0 || c.y == voxels[i].Length - 1 ||
+                    c.z == 0 || c.z == voxels[i][j].Length - 1)
+                    {
+                        voxels[i][j][k].GetComponent<Voxel>().type = Voxel.Type.SURFACE;
+                        voxels[i][j][k].AddComponent<BoxCullider>().updateBoundaries();
+                    }
+                    else
+                    {
+                        voxels[i][j][k].GetComponent<Voxel>().type = Voxel.Type.INTERIOR;
+                    }
                 }
             }
         }
