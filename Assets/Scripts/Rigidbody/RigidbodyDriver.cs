3b1f40913ca1c19f8dea4f047b857fbcfadb0930
Mohammad Issawi <2000issawi@gmail.com>
2023-04-28 21:44:20 +0300

    Apply angular velocity to a rotation

--- Assets/Scripts/Rigidbody/RigidbodyDriver.cs
+++ Assets/Scripts/Rigidbody/RigidbodyDriver.cs
@@ -6,6 +6,7 @@ public class RigidbodyDriver : MonoBehaviour
 {
     private Rigidbody rb;
     private Vector3 velocity;
+    private Quaternion angularVelocity;
     private static Vector3 gravity = new Vector3(0, -9.8f, 0);
     void Start()
     {
@@ -17,6 +18,29 @@ public class RigidbodyDriver : MonoBehaviour
         if (rb.useGravity)
             velocity += gravity * Time.fixedDeltaTime;
         transform.position += velocity * Time.fixedDeltaTime;
+
+        transform.rotation = quatQuatAdd(transform.rotation,
+                            floatQuatMult(0.5f * Time.fixedDeltaTime,
+                            (angularVelocity * transform.rotation)));
+
+    }
+
+    public static Quaternion floatQuatMult(float f, Quaternion quat)
+    {
+        quat[0] *= f;
+        quat[1] *= f;
+        quat[2] *= f;
+        quat[3] *= f;
+        return quat;
+    }
+
+    public static Quaternion quatQuatAdd(Quaternion quat0, Quaternion quat1)
+    {
+        quat0[0] += quat1[0];
+        quat0[1] += quat1[1];
+        quat0[2] += quat1[2];
+        quat0[3] += quat1[3];
+        return quat0;
     }
 
     public void applyLinearMomentum(RigidbodyDriver other)
