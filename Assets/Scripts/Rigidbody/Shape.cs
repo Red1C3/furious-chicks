f95b07cc531ba2304aa83db4b7ff58d04a7ce706
Mohammad Issawi <2000issawi@gmail.com>
2023-05-01 23:26:00 +0300

    Fix rotation direction

--- Assets/Scripts/Rigidbody/Shape.cs
+++ Assets/Scripts/Rigidbody/Shape.cs
@@ -6,9 +6,9 @@ public interface Shape{
     Vector3 center();
     static float inertiaScalar(float3x3 inertiaTensor,float3 axis){
         if(math.all(axis==float3.zero)){
-            return 0.00000000000000001f;
+            return math.EPSILON;
         }
         axis=math.normalize(axis);
-        return math.mul(math.mul(axis,inertiaTensor),axis);
+        return math.abs(math.mul(math.mul(axis,inertiaTensor),axis));
     }
 }
--- Assets/Scripts/Solvers/ImpulseSolver.cs
+++ Assets/Scripts/Solvers/ImpulseSolver.cs
@@ -11,21 +11,21 @@ public class ImpulseSolver : Solver
         Vector3 rA = cullision.contactPointA - cullision.first.center();
         Vector3 rB = cullision.contactPointB - cullision.second.center();
 
-        float12 jacobian = new float12(-normal, Vector3.Cross(-normal, rA)
-                                    , normal, Vector3.Cross(normal, rB));
+        float12 jacobian = new float12(-normal, Vector3.Cross(normal, rA)
+                                    , normal, Vector3.Cross(-normal, rB));
 
         float12x12 inverseMass = new float12x12(1.0f / cullision.first.getRigidbody().mass,
-                                        1.0f / Shape.inertiaScalar(cullision.first.getTensorInertia(), (Vector3.Cross(-normal, rA))),
+                                        1.0f / Shape.inertiaScalar(cullision.first.getTensorInertia(), (Vector3.Cross(normal, rA))),
                                         1.0f / cullision.second.getRigidbody().mass,
-                                        1.0f / Shape.inertiaScalar(cullision.second.getTensorInertia(), (Vector3.Cross(normal, rB))));
+                                        1.0f / Shape.inertiaScalar(cullision.second.getTensorInertia(), (Vector3.Cross(-normal, rB))));
 
         float12 velocities=new float12(cullision.first.getRigidbodyDriver().velocity,
                                         cullision.first.getRigidbodyDriver().getAngularVelocity(),
                                         cullision.second.getRigidbodyDriver().velocity,
                                         cullision.second.getRigidbodyDriver().getAngularVelocity());
 
-        float lambda=(float12x12.rowColMult(jacobian,velocities))/(float12x12.rowColMult((jacobian*inverseMass),jacobian));
-        lambda=math.clamp(lambda,0,float.MaxValue);
+        float lambda=-(float12x12.rowColMult(jacobian,velocities))/(float12x12.rowColMult((jacobian*inverseMass),jacobian));
+        lambda=math.clamp(lambda,float.MinValue,0);
 
         float12 deltaV=inverseMass*jacobian*lambda;
 
