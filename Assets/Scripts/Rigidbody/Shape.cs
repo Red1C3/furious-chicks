37cfefd6b539dcd7d5e463969724925988f83784
Mohammad Issawi <2000issawi@gmail.com>
2023-05-01 19:24:26 +0300

    calculate inverse mass matrix

--- Assets/Scripts/Rigidbody/Shape.cs
+++ Assets/Scripts/Rigidbody/Shape.cs
@@ -4,4 +4,8 @@ using Unity.Mathematics;
 public interface Shape{
     float3x3 getTensorInertia();
     Vector3 center();
+    static float inertiaScalar(float3x3 inertiaTensor,float3 axis){
+        axis=math.normalize(axis);
+        return math.mul(math.mul(axis,inertiaTensor),axis);
+    }
 }
--- Assets/Scripts/Solvers/ImpulseSolver.cs
+++ Assets/Scripts/Solvers/ImpulseSolver.cs
@@ -5,14 +5,19 @@ public class ImpulseSolver : Solver
 {
     public override void resolveCullision(CullisionInfo cullision, Rigidbody A, Rigidbody B)
     {
-        if(!cullision.cullided) return;
-        Vector3 normal=cullision.normal.normalized*cullision.depth;
+        if (!cullision.cullided) return;
+        Vector3 normal = cullision.normal.normalized * cullision.depth;
 
-        Vector3 rA=cullision.contactPointA-cullision.first.center();
-        Vector3 rB=cullision.contactPointB-cullision.second.center();
+        Vector3 rA = cullision.contactPointA - cullision.first.center();
+        Vector3 rB = cullision.contactPointB - cullision.second.center();
 
-        float12 jacobian=new float12(-normal,Vector3.Cross(-normal,rA)
-                                    ,normal,Vector3.Cross(normal,rB));
+        float12 jacobian = new float12(-normal, Vector3.Cross(-normal, rA)
+                                    , normal, Vector3.Cross(normal, rB));
+
+        float12x12 inverseMass = new float12x12(1.0f / cullision.first.getRigidbody().mass,
+                                        1.0f / Shape.inertiaScalar(cullision.first.getTensorInertia(), (Vector3.Cross(-normal, rA))),
+                                        1.0f / cullision.second.getRigidbody().mass,
+                                        1.0f / Shape.inertiaScalar(cullision.second.getTensorInertia(), (Vector3.Cross(normal, rB))));
 
         
     }
