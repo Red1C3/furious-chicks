ff59fe5100c9634218f3402219a3bc02f91f517e
Mohammad Issawi <2000issawi@gmail.com>
2023-05-05 13:47:45 +0300

    Implement a part of SH algor

--- Assets/Scripts/Geometry/Edge.cs
+++ Assets/Scripts/Geometry/Edge.cs
@@ -1,10 +1,49 @@
 using UnityEngine;
 
-public struct Edge{
-    public Vector3 from{get; private set;}
-    public Vector3 to{get; private set;}
-    public Edge(Vector3 from,Vector3 to){
-        this.from=from;
-        this.to=to;
+public struct Edge
+{
+    public Vector3 from { get; private set; }
+    public Vector3 to { get; private set; }
+    public Edge(Vector3 from, Vector3 to)
+    {
+        this.from = from;
+        this.to = to;
+    }
+    public void toLocal(Transform transform)
+    {
+        from = transform.InverseTransformPoint(from);
+        to = transform.InverseTransformPoint(to);
+    }
+
+    public Vector3[] clip(Edge clipper)
+    {
+        float fromSign = (clipper.to.x - clipper.from.x) * (from.y - clipper.from.y) -
+                        (clipper.to.y - clipper.from.y) * (from.x - clipper.from.x);
+
+        float toSign = (clipper.to.x - clipper.from.x) * (to.y - clipper.from.y) -
+                        (clipper.to.y - clipper.from.y) * (to.x - clipper.from.x);
+
+        //Both vertices are inside the clipping area
+        if (fromSign <= 0 && toSign <= 0)
+        {
+            return new[] { to };
+        }
+        else if (toSign <= 0)
+        { //Only the 2nd vertex is inside
+          //return both the intersection point between the edges and to vertex
+            return new Vector3[] { }; //TODO
+
+        }
+        else if (fromSign <= 0)
+        {
+            //return only the point of intersection
+            return new Vector3[] { }; //TODO
+
+        }
+        else
+        {
+            return new Vector3[] { };
+        }
+
     }
 }
--- Assets/Scripts/Geometry/Face.cs
+++ Assets/Scripts/Geometry/Face.cs
@@ -14,7 +14,7 @@ public struct Face
             edges[1] = new Edge(vertices[1], vertices[2]);
             edges[2] = new Edge(vertices[2], vertices[0]);
 
-            if (flipWinding)
+            if (flipWinding) //Unity's default is CCW
             {
                 Edge tempEdge = edges[0];
                 edges[0] = edges[2];
@@ -36,8 +36,28 @@ public struct Face
     }
 
     public Face clip(Transform transform){
+        if(winding==Winding.CCW){
+            Debug.Log("Can't clip CCW winded faces");
+            return this;
+        }
+        Edge[] clippingArea=new Edge[4]; //Can be computed once by making it static
+        clippingArea[0]=new Edge(new Vector2(-1,-1),new Vector2(-1,1));// Must be CW
+        clippingArea[1]=new Edge(new Vector2(-1,1),new Vector2(1,1));
+        clippingArea[2]=new Edge(new Vector2(1,1),new Vector2(1,-1));
+        clippingArea[3]=new Edge(new Vector2(1,-1),new Vector2(-1,-1));
+
         Face clipped=this; //A copy
         clipped.edges=(Edge[])edges.Clone();
+
+        clipped.toLocal(transform);
+
+
         return clipped;//TODO
+    }
+
+    public void toLocal(Transform transform){
+        for(int i=0;i<edges.Length;i++){
+            edges[i].toLocal(transform);
+        }
     }
 }
