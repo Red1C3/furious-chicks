5ff52596fb70a43f90b6f8a5513dd4fb4f6606d7
Mohammad Issawi <2000issawi@gmail.com>
2023-06-03 19:31:48 +0300

    Setup throwing phase for first bird

--- Assets/Scripts/Game/LevelCtrlr.cs
+++ Assets/Scripts/Game/LevelCtrlr.cs
@@ -8,12 +8,24 @@ public class LevelCtrlr : MonoBehaviour
     private BirdBase[] birds;
     private int currentBird = 0;
     private bool throwingPhase = true;
+    [SerializeField]
+    private GameObject linePrefab;
+    private LineRenderer line;
+
+    private void Start()
+    {
+        line = Instantiate(linePrefab, Vector3.zero, Quaternion.identity).GetComponent<LineRenderer>();
+    }
+
 
     private void Update()
     {
         if (throwingPhase)
         {
-            //TODO
+            Throw throwInst = birds[currentBird].gameObject.AddComponent<Throw>();
+            throwInst.lineRenderer = line;
+            throwInst.cam = Camera.main;
+            throwingPhase=false;
         }
         else
         {
