b8fc17ebf4a961b616ca50fe445717384e8eca29
Maias-Alakramy <maias-alakramy01@hotmail.com>
2023-06-05 20:43:49 +0300

    new way to do ability

--- Assets/Scripts/Game/RedBird.cs
+++ Assets/Scripts/Game/RedBird.cs
@@ -4,19 +4,7 @@ using UnityEngine;
 
 public class RedBird : BirdBase
 {
-    bool once=true;
-    protected void ability(){
+    protected override void ability(){
         addForce(velocity*100,ForceMode.Impulse);
-    }
-
-    public void Update(){
-        if(hasFired){
-            Debug.Log(velocity.z);
-            if(Input.GetKeyDown(KeyCode.F)){
-                Debug.Log("pressed");
-                ability();
-                Debug.Log("pressed");
-            }
-        }
     }
 }
