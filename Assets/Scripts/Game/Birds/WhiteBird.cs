99d166a06f30758750f823ec8a3be727d1e85413
Maias-Alakramy <maias-alakramy01@hotmail.com>
2023-06-07 20:43:36 +0300

    White bird V0.2

--- Assets/Scripts/Game/Birds/WhiteBird.cs
+++ Assets/Scripts/Game/Birds/WhiteBird.cs
@@ -6,13 +6,11 @@ public class WhiteBird : BirdBase
 {
     [SerializeField]
     private GameObject eggPrefab;
-    [SerializeField]
-    private LevelCtrlr lvlctr;
 
     private GameObject egg;
     protected override void ability(){
         Vector3 eggPos = transform.position + new Vector3(0.0f,-0.5f,0.2f);
         egg = Instantiate(eggPrefab, eggPos, Quaternion.identity);
-        lvlctr.engine.addCullider(egg);
+        levelCtrlr.engine.addCullider(egg);
     }
 }
