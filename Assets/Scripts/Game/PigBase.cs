8c3aa96473c2fa9aeb522f4729838981174c1e49
Mohammad Issawi <2000issawi@gmail.com>
2023-06-07 23:57:50 +0300

    Take both colliding objects getDamage values into account

--- Assets/Scripts/Game/PigBase.cs
+++ Assets/Scripts/Game/PigBase.cs
@@ -4,12 +4,13 @@ public class PigBase : FCObject
 {
     [SerializeField]
     protected int health = 100;
+    protected static new int firstDamage = 0;
     public override void onCullisionEnter(Cullider other)
     {
         if (other.getRigidbodyDriver() is FCObject)
         {
             FCObject otherFC = (FCObject)other.getRigidbodyDriver();
-            health -= otherFC.getDamage();
+            health -= (otherFC.getDamage() + getDamage()) / 2;
             if (health <= 0) levelCtrlr.destroyPig(this);
         }
     }
