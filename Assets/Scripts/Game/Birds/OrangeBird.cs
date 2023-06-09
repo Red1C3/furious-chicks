c52c064a2ad7a383b4766dc7672c434ea81653d5
Mohammad Issawi <2000issawi@gmail.com>
2023-06-09 21:45:33 +0300

    Readjust gravity after increasing the mass of the orange bird

--- Assets/Scripts/Game/Birds/OrangeBird.cs
+++ Assets/Scripts/Game/Birds/OrangeBird.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class OrangeBird : BirdBase
 {
     float maxScale, updateRate;
-    protected override void Start(){
+    protected override void Start()
+    {
         base.Start();
-        maxScale=transform.localScale.x;
-        updateRate=1.0e-2f;
+        maxScale = transform.localScale.x;
+        updateRate = 1.0e-2f;
     }
     protected override void ability()
     {
-        maxScale*=4;
+        maxScale *= 4;
     }
 
     protected void Update()
     {
         base.Update();
-        if(!hasAbility && transform.localScale.x<=maxScale){
+        if (!hasAbility && transform.localScale.x <= maxScale)
+        {
             transform.localScale += new Vector3(1, 1, 1) * updateRate;
-            mass += 1 * updateRate * mass;
+            float newMass = mass + updateRate * mass;
+            addForce(gravity * updateRate * mass, ForceMode.Force);
+            mass = newMass;
             levelCtrlr.currentBirdThrow.cameraAway += 2 * updateRate;
             GetComponent<SphereCullider>().updateRadius();
         }
