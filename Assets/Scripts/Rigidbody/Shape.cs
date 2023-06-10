84469b9c8976fc89c1ed3d6b6bbc2e9537f71d30
Mohammad Issawi <2000issawi@gmail.com>
2023-06-10 20:35:47 +0300

    Save up unnesccary (hopefully) math.abs in Shape.inertiaScalar

--- Assets/Scripts/Rigidbody/Shape.cs
+++ Assets/Scripts/Rigidbody/Shape.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using Unity.Mathematics;
 
-public interface Shape{
-    float3x3 getTensorInertia(); //TODO cache
+public interface Shape
+{
+    float3x3 getTensorInertia();
     Vector3 center();
-    static float inertiaScalar(float3x3 inertiaTensor,float3 axis){
-        if(math.all(axis==float3.zero)){
+    static float inertiaScalar(float3x3 inertiaTensor, float3 axis)
+    {
+        if (math.all(axis == float3.zero))
+        {
             return math.EPSILON;
         }
-        axis=math.normalize(axis);
-        return math.abs(math.mul(math.mul(axis,inertiaTensor),axis));
+        axis = math.normalize(axis);
+        return math.mul(math.mul(axis, inertiaTensor), axis);
     }
 }
