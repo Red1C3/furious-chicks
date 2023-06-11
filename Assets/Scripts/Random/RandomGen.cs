b5ee0cc411bf29f1ffde47592e509aaeea915efc
Maias-Alakramy <maias-alakramy01@hotmail.com>
2023-06-11 20:59:02 +0300

    Trash V2

--- Assets/Scripts/Random/RandomGen.cs
+++ Assets/Scripts/Random/RandomGen.cs
@@ -19,60 +19,42 @@ public class RandomGen : MonoBehaviour
     {
         copyNumber=objectNumber;
 
-        ground.transform.localScale = new Vector3(2*groundScale,0.1f,2*groundScale);
-        ground.transform.position = new Vector3(0,0,groundScale*3.0f/2.0f);
-        heights = new List<List<float>>();
-        for (int i = 0; i < groundScale; i++)
-        {
-            heights.Add(new List<float>());
-            for (int j = 0; j < groundScale; j++){
-                heights[i].Add(0);
-            }
-        }
-        float sum=0;
-        for (int i = 0; i < blocksPros.Length; i++)
-        {
-            sum+=blocksPros[i];
-            blocksPros[i]=sum;
-        }
-        for (int i = 0; i < blocksPros.Length; i++)
-        {
-            blocksPros[i]/=sum;
-        }
+        initGround();
+
+        initHeights();
+
+        calcPros();
+        
         for (int i = 0; i < copyNumber; i++)
         {
-            scale=new Vector3(Random.Range(1, 6),Random.Range(1, 6),Random.Range(1, 6));
+            scale=new Vector3(2*Random.Range(1, 3)+1,2*Random.Range(1, 3)+1,2*Random.Range(1, 3)+1);
 
             Vector3 pos = getCenterVector();
             if(copyNumber<=0)
                 break;
             GameObject randPrefab = Instantiate(blocksPrefabs[randomPrefabIndex()], pos , Quaternion.identity);
-            randPrefab.transform.localScale = scale;            
+            randPrefab.transform.localScale = scale;      
+            randPrefab.GetComponent<RigidbodyDriver>().mass = calcMass(scale);
+
         }
         
     }
 
-    public int randomPrefabIndex(){
-        float r = Random.value;
-        int i=0;
-        for(;i<blocksPrefabs.Length;i++){
-            if(blocksPros[i]>=r)
-                break;
-        }
-        return i;
-    }
-
-    public Vector3 getCenterVector(){
+    private Vector3 getCenterVector(){
         int x = (int) Random.Range(0,groundScale);
         int z = (int) Random.Range(0,groundScale);
+        
+        // int x = (int) Random.Range((int)scale.x/2.0f,groundScale-((int)scale.x/2.0f));
+        // int z = (int) Random.Range((int)scale.z/2.0f,groundScale-((int)scale.z/2.0f));
         if(scale.x==scale.z && scale.x==1){
             heights[x][z]+=scale.y;
             return new Vector3(x-(int) (groundScale/2.0f), heights[x][z]-scale.y, z-(int) (groundScale/2.0f))+0.5f*scale+ground.transform.position;
         }
 
         else if(scale.x==1){
-            float dz = Mathf.Abs(scale.z/2.0f-0.5f);
+            int dz = (int) Mathf.Abs(scale.z/2.0f);
             int z1=(int)Mathf.Max(z-dz,0),z2=(int)Mathf.Min(z+dz,groundScale-1);
+
             float newH = heights[x][z];
             for(int i=z1;i<=z2;i++)
                 newH = Mathf.Max(newH,heights[x][i]);
@@ -84,8 +66,9 @@ public class RandomGen : MonoBehaviour
         }
 
         else if(scale.z==1){
-            float dx = Mathf.Abs(scale.x/2.0f-0.5f);
+            int dx =(int) Mathf.Abs(scale.x/2.0f);
             int x1=(int)Mathf.Max(x-dx,0),x2=(int)Mathf.Min(x+dx,groundScale-1);
+
             float newH = heights[x][z];
             for(int j=x1;j<=x2;j++)
                 newH = Mathf.Max(newH,heights[j][z]);
@@ -96,10 +79,10 @@ public class RandomGen : MonoBehaviour
             return new Vector3(x-(int) (groundScale/2.0f), heights[x][z]-scale.y, z-(int) (groundScale/2.0f))+0.5f*scale+ground.transform.position;
         }
         else{
-            float dx = Mathf.Abs(scale.x/2.0f-0.5f);
+            int dx =(int) Mathf.Abs(scale.x/2.0f);
             int x1=(int)Mathf.Max(x-dx,0),x2=(int)Mathf.Min(x+dx,groundScale-1);
             
-            float dz = Mathf.Abs(scale.z/2.0f-0.5f);
+            int dz =(int) Mathf.Abs(scale.z/2.0f);
             int z1=(int)Mathf.Max(z-dz,0),z2=(int)Mathf.Min(z+dz,groundScale-1);
             
             float newH = heights[x][z];
@@ -117,15 +100,68 @@ public class RandomGen : MonoBehaviour
         }
     }
 
-    public void makePilier(float maxH, int x, int z){
+    private void makePilier(float maxH, int x, int z){
         float y = maxH - heights[x][z];
         if (y<=1e-8 || copyNumber<=0){
             return;
         }
-        GameObject pilier = Instantiate(blocksPrefabs[randomPrefabIndex()],new Vector3(x-(int) (groundScale/2.0f),y/2,z-(int) (groundScale/2.0f))+ground.transform.position , Quaternion.identity);
-        pilier.transform.localScale = new Vector3(1,y,1);
+        Vector3 pilierScale = new Vector3(1,y,1);
+        heights[x][z]+=pilierScale.y;
+        Vector3 pos = new Vector3(x-(int) (groundScale/2.0f), heights[x][z]-pilierScale.y, z-(int) (groundScale/2.0f))+0.5f*pilierScale+ground.transform.position;
+        
+        GameObject pilier = Instantiate(blocksPrefabs[randomPrefabIndex()],pos , Quaternion.identity);
+        pilier.transform.localScale = pilierScale;
+        pilier.GetComponent<RigidbodyDriver>().mass = calcMass(pilierScale);
+
         pilier.tag = "Pilier";
         copyNumber--;
         return;
     }
+
+    private void initHeights(){
+        heights = new List<List<float>>();
+        for (int i = 0; i < groundScale; i++)
+        {
+            heights.Add(new List<float>());
+            for (int j = 0; j < groundScale; j++){
+                heights[i].Add(0);
+            }
+        }
+    }
+
+    private void initGround(){
+        ground.transform.localScale = new Vector3(2*groundScale,0.1f,2*groundScale);
+        ground.transform.position = new Vector3(0,0,groundScale*3.0f/2.0f);
+    }
+
+    private void calcPros(){
+        float sum=0;
+        for (int i = 0; i < blocksPros.Length; i++)
+        {
+            sum+=blocksPros[i];
+            blocksPros[i]=sum;
+        }
+        for (int i = 0; i < blocksPros.Length; i++)
+        {
+            blocksPros[i]/=sum;
+        }
+    }
+
+    private int randomPrefabIndex(){
+        float r = Random.value;
+        int i=0;
+        for(;i<blocksPrefabs.Length;i++){
+            if(blocksPros[i]>=r)
+                break;
+        }
+        return i;
+    }
+
+    private float calcMass(Vector3 scale){
+        float newMass = 1;
+        newMass*=scale.x;
+        newMass*=scale.y;
+        newMass*=scale.z;
+        return newMass;
+    }    
 }
