8aa7baa801700bab6095e2907bd678b9259dc65f
Mohammad Issawi <2000issawi@gmail.com>
2023-06-15 21:40:56 +0300

    Create main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu:MonoBehaviour
+{
+    public static void level1()
+    {
+        SceneManager.LoadScene("Level-1");
+    }
+    public static void level2()
+    {
+        SceneManager.LoadScene("Level-2");
+    }
+    public static void levelGen()
+    {
+        SceneManager.LoadScene("Level-Gen");
+    }
+}
