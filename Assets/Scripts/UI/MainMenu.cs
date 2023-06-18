31b5d5b43ac945a3aad41fff600c459214b5f2d3
Mohammad Issawi <2000issawi@gmail.com>
2023-06-18 18:34:38 +0300

    Quit App on escape

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -17,4 +17,9 @@ public class MainMenu:MonoBehaviour
     {
         SceneManager.LoadScene("Level-Gen");
     }
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Application.Quit();
+        }
+    }
 }
